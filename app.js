e80ef657234dddbc97cce9845d12fa086260a1c9
koebe1 <koe.be.pay@web.de>
2020-05-24 11:28:27 +0200

    added default placeholder when deleting input

--- app.js
+++ app.js
@@ -6,53 +6,49 @@ const kelvinInput = document.querySelector('.kelvin');
 celsiusInput.addEventListener('input', () => {
     let celToFa = celsiusToFahrenheit(celsiusInput.value);
     let celToKel = celsiusToKelvin(celsiusInput.value);
-    fahrenheitInput.placeholder = celToFa;
-    kelvinInput.placeholder = celToKel;
+    fahrenheitInput.value = celToFa;
+    kelvinInput.value = celToKel;
 });
 
 fahrenheitInput.addEventListener('input', () => {
     let faToCel = fahrenheitToCelsius(fahrenheitInput.value);
     let faToKel = fahrenheitToKelvin(fahrenheitInput.value);
-    celsiusInput.placeholder = faToCel;
-    kelvinInput.placeholder  = faToKel;
+    celsiusInput.value = faToCel;
+    kelvinInput.value  = faToKel;
 });
 
 kelvinInput.addEventListener('input', () => {
     let kelToCel = kelvinToCelsius(kelvinInput.value);
     let kelToFa = kelvinToFahrenheit(kelvinInput.value);
-    celsiusInput.placeholder = kelToCel;
-    fahrenheitInput.placeholder = kelToFa;
+    celsiusInput.value = kelToCel;
+    fahrenheitInput.value = kelToFa;
 });
-
-
-
-
 
 
 
 
 // converting formulas
 const celsiusToFahrenheit = (cel) => {
-    return Math.round((((parseInt(cel) * 9 / 5) + 32) + Number.EPSILON) *100) /100;
+    return Math.round((((parseFloat(cel) * 9 / 5) + 32) + Number.EPSILON) *100) /100;
 }
 
 const celsiusToKelvin = (cel) => {
-    return Math.round(((parseInt(cel) + 273.15) + Number.EPSILON) *100) /100;
+    return Math.round(((parseFloat(cel) + 273.15) + Number.EPSILON) *100) /100;
 }
 
 const kelvinToCelsius = (kel) => {
-    return Math.round(((parseInt(kel) - 273.15) + Number.EPSILON) *100) /100;
+    return Math.round(((parseFloat(kel) - 273.15) + Number.EPSILON) *100) /100;
 }
 
 const kelvinToFahrenheit = (kel) => {
-    return Math.round((((parseInt(kel) - 273.15) * 9 / 5 + 32) + Number.EPSILON) *100) /100;
+    return Math.round((((parseFloat(kel) - 273.15) * 9 / 5 + 32) + Number.EPSILON) *100) /100;
 }
 
 const fahrenheitToCelsius = (fa) => {
-    return Math.round((((parseInt(fa)-32) * 5/9) + Number.EPSILON) *100) /100;
+    return Math.round((((parseFloat(fa)-32) * 5/9) + Number.EPSILON) *100) /100;
 }
 
 const fahrenheitToKelvin = (fa) => {
-    return Math.round((((parseInt(fa)-32) * 5/9 + 273.15) + Number.EPSILON) *100) /100;
+    return Math.round((((parseFloat(fa)-32) * 5/9 + 273.15) + Number.EPSILON) *100) /100;
 }
 
